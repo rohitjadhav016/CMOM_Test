b2f57446ae46369d42027c8d3059d6ec752ac78f
rohitjadhav016 <jrohit016@gmail.com>
2022-02-24 13:19:38 +0000

    Second Commit

--- src/component/GetGeckoApi.js
+++ src/component/GetGeckoApi.js
@@ -37,9 +37,10 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 const GetGeckoApi = () => {
 //defining the required const and variables
 const [marketTableData, setmarketTableData] = useState([]);
-const[search, setSearchData] = useState(null);
-let marketDataCols = [];
-let url = "";
+const [marketSearchData, setmarketSearchData] = useState([]);
+const[search, setSearch] = useState(null); 
+let marketDataCols, marketDataSearchCols = [];
+let url, searchUrl = "";
 let errorMessage = "";
 marketDataCols = [
   {field: "image", headerName: "Image"},
@@ -53,62 +54,79 @@ url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=mark
 
 //setting the url based on input by user
 if(search){
-  marketDataCols = [
+  let keyword = search.toLowerCase();
+  searchUrl = `https://api.coingecko.com/api/v3/coins/${keyword}`;
+  marketDataSearchCols = [
     {field: "name", headerName: "Name"},
     {field: "symbol", headerName: "Symbol"},
     {field: "hashing_algorithm", headerName: "Hashing Algorithm"},
     {field: "description", headerName: "Description"},
+    {field: "genesis_date", headerName: "Genesis Date"},
     {field: "coingecko_score", headerName: "Market Cap in Euro"},
     {field: "homepage", headerName: "Homepage"},
-  ];
-  let keyword = search.toLowerCase();
-  url = `https://api.coingecko.com/api/v3/coins/${keyword}`;
+  ]
+
 }
 
 //get data from the api
-const getMarketData = async () => {
-  alert(url);
-  const response = await fetch(url);
-  const data = await response.json();
-  if(!data){
-    errorMessage = 'No Data Found';
-  }
-  console.log(data);
+ const getMarketData = async () => {
+   const response = await fetch(url);
+   const data = await response.json();
+  if(data){
   setmarketTableData(data);
   }
-
-function soemDate(data) {
-  return Object.values(data).map((curElem) => (console.log(curElem)));
 }
 
-// function displayData(data, search){
-//   return(
-//     data.map((curElem) => (
-//       <StyledTableRow>
-//         {!curElem.image && search ? (
-//         <>
-//           <StyledTableCell align="center">{curElem.name}</StyledTableCell>
-//           <StyledTableCell align="center">{curElem.symbol}</StyledTableCell>
-//           <StyledTableCell align="center">{curElem.hashing_algorithm}</StyledTableCell>
-//           <StyledTableCell align="center">{curElem.coingecko_score}</StyledTableCell>
-//           <StyledTableCell align="center">{curElem.low_24h}</StyledTableCell>
-//           <StyledTableCell align="center">{curElem.low_24h}</StyledTableCell>
-//         </>
-//         ):(
-//         <>
-//           <StyledTableCell align="center"><img src={curElem.image} alt="Icon" height={25} width={25}/></StyledTableCell>
-//           <StyledTableCell align="center">{curElem.name}</StyledTableCell>
-//           <StyledTableCell align="center">{curElem.symbol}</StyledTableCell>
-//           <StyledTableCell align="center">{curElem.current_price}</StyledTableCell>
-//           <StyledTableCell align="center">{curElem.high_24h}</StyledTableCell>
-//           <StyledTableCell align="center">{curElem.low_24h}</StyledTableCell>
-//         </>
-//         )}
-//       </StyledTableRow>
-//       ))
-//   )              
-// }
+function displayData(data){
+return(
+  data.map((curElem) => (
+    <StyledTableRow>
+      <>
+        <StyledTableCell align="center"><img src={curElem.image} alt="Icon" height={25} width={25}/></StyledTableCell>
+        <StyledTableCell align="center">{(curElem.name) ? curElem.name : ''}</StyledTableCell>
+        <StyledTableCell align="center">{(curElem.symbol) ? curElem.symbol : ''}</StyledTableCell>
+        <StyledTableCell align="center">{(curElem.current_price) ? curElem.current_price : ''}</StyledTableCell>
+        <StyledTableCell align="center">{(curElem.high_24h) ? curElem.high_24h : ''}</StyledTableCell>
+        <StyledTableCell align="center">{(curElem.low_24h) ? curElem.low_24h : ''}</StyledTableCell>
+      </>
+    </StyledTableRow>
+    ))
+  )              
+}
 
+  //get search data from the api
+const getSearchData = async () => {
+
+  const response = await fetch(searchUrl);
+  const data = await response.json();
+  if(data){
+    setmarketSearchData(data);
+    // eslint-disable-next-line no-const-assign
+    }else{
+      errorMessage = 'No Data Found';
+    }
+  
+  }
+
+
+function displaySearchData(data){
+  if(data){
+    var values = Object.values(data);
+  }
+  return(     
+    <StyledTableRow>
+      <>
+        <StyledTableCell align="center">{(values) ? values[2] : ''}</StyledTableCell>
+        <StyledTableCell align="center">{(values) ? values[0] : ''}</StyledTableCell>
+        <StyledTableCell align="center">{(values) ? values[6] : ''}</StyledTableCell>
+        <StyledTableCell align="center"><p>{(values) ? values[11]['en'] : ''}</p></StyledTableCell>
+        <StyledTableCell align="center">{(values) ? values[15] : ''}</StyledTableCell>
+        <StyledTableCell align="center">{(values) ? values[25]['market_cap']['eur'] : ''}</StyledTableCell>
+        <StyledTableCell align="center">{(values) ? values[12]['homepage'][0] : ''}</StyledTableCell>
+      </>
+    </StyledTableRow>
+  )
+}
 
 useEffect(() => {
 getMarketData();
@@ -117,28 +135,47 @@ getMarketData();
 
 return (
 <div className="container">
-    <div className="header-div">
-        <h2>Welcome to CoinGecko CryptoCurrency Tracker</h2>
-        <p> Following API Results are ordered by <strong>Market Cap Description</strong> and using Euro as default <strong>VS_Currency</strong> </p>
-        <p>Please Enter the Coin ID below to get the results by Coin ID</p>
-    </div>
-    <input type="search" placeholder="Eg: bitcoin, ethereum" onChange={(event)=>{setSearchData(event.target.value)}}></input>
+  <div className="header-div">
+      <h2>Welcome to CoinGecko CryptoCurrency Tracker</h2>
+      <p> Following API Results are ordered by <strong>Market Cap Description</strong> and using Euro as default <strong>VS_Currency</strong> </p>
+      <p>Please Enter the Coin ID below to get the results by Coin ID</p>
+  </div>
+  <input type="search" placeholder="Eg: bitcoin, ethereum" autoComplete="off" onChange={(event) => {setSearch(event.target.value);}}></input>
+  <button className="btn-submit" onClick={() => {getSearchData()}}>Submit</button>
+
+  <div>
+    {(marketSearchData || marketTableData) ? 
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
-          {!marketTableData && !search ? (<p style={{color:'red'}}> {errorMessage} </p>) : (marketDataCols.map((curElem) => (
-            <StyledTableCell align="center">{curElem.headerName}</StyledTableCell>
-          )))}
+          {
+            (marketDataCols)?
+            (marketDataCols.map((curElem) => (
+              <StyledTableCell align="center">{curElem.headerName}</StyledTableCell>
+            )))
+            :
+            (marketDataSearchCols.map((curElem) => (
+              <StyledTableCell align="center">{curElem.headerName}</StyledTableCell>
+            )))
+          }
           </TableRow>
         </TableHead>
       <TableBody>
-        {soemDate(marketTableData)}
+      {
+        (marketTableData)?
+        displayData(marketTableData) : 
+        displaySearchData(marketSearchData)
+        
+      }
         </TableBody> 
       </Table>
-    </TableContainer>
+  </TableContainer>
+  :
+  <p className='errorMessage'> {errorMessage} </p>
+  }
+  </div>
 </div>  
-
   )
 }
 export default GetGeckoApi;
